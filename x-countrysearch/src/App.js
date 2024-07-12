bae208008c2afb84e4833badb769e8d3c640db01
Susovan <susovan.sahoo777@gmail.com>
2024-07-12 10:39:06 +0530

    Added API data fetching and prited in the console

--- x-countrysearch/src/App.js
+++ x-countrysearch/src/App.js
@@ -1,9 +1,29 @@
-import './App.css';
+import "./App.css";
+import { useState, useEffect } from "react";
 
 function App() {
+  const [country, setCountry] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch("https://restcountries.com/v3.1/all");
+        const data = await response.json();
+        setCountry(data);
+      } catch (e) {
+        console.error("Error fetching data: " + e);
+      }
+    };
+
+    fetchData();
+  }, []);
+  console.log(country);
   return (
     <div className="App">
-      
+      <div className="search">
+        <input type="text" placeholder="Search for countries..." />
+      </div>
+      <div className="Result"></div>
     </div>
   );
 }
