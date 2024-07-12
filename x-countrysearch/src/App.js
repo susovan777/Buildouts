de1a01b98b23c47563b321f50a55a3cada879e5e
Susovan <susovan.sahoo777@gmail.com>
2024-07-12 10:54:48 +0530

    Added country flags to the UI

--- x-countrysearch/src/App.js
+++ x-countrysearch/src/App.js
@@ -2,14 +2,14 @@ import "./App.css";
 import { useState, useEffect } from "react";
 
 function App() {
-  const [country, setCountry] = useState([]);
+  const [countries, setCountries] = useState([]);
 
   useEffect(() => {
     const fetchData = async () => {
       try {
         const response = await fetch("https://restcountries.com/v3.1/all");
         const data = await response.json();
-        setCountry(data);
+        setCountries(data);
       } catch (e) {
         console.error("Error fetching data: " + e);
       }
@@ -17,13 +17,22 @@ function App() {
 
     fetchData();
   }, []);
-  console.log(country);
+  console.log(countries);
   return (
     <div className="App">
       <div className="search">
         <input type="text" placeholder="Search for countries..." />
       </div>
-      <div className="Result"></div>
+      <div className="Result">
+        {countries.map((country) => {
+          return (
+            <div className="countryCard">
+              <img src={country.flags.png} alt="" />
+              <h3>{country.name.common}</h3>
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 }
