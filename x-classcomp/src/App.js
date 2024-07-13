c8b95c5de82438cbca85b115edce7e39d95b1405
Susovan <susovan.sahoo777@gmail.com>
2024-07-13 23:33:01 +0530

    Added new buildout x-classcomp

---
+++ x-classcomp/src/App.js
@@ -0,0 +1,32 @@
+import { Component } from "react";
+import "./App.css";
+
+class App extends Component {
+  state = {
+    count: 0,
+  };
+
+  increment = () => {
+    this.setState({
+      count: this.state.count + 1,
+    });
+  };
+
+  decrement = () => {
+    this.setState({
+      count: this.state.count - 1,
+    });
+  };
+
+  render() {
+    return (
+      <div>
+        <h1>Counter App</h1>
+        <p>Count: {this.state.count}</p>
+        <button onClick={this.increment}>Increment</button>
+        <button onClick={this.decrement}>Decrement</button>
+      </div>
+    );
+  }
+}
+export default App;
