0e5e377f57e0ebc76b7a8ee6d520a53533029328
Susovan <susovan.sahoo777@gmail.com>
2024-07-26 08:07:20 +0530

    Final submit of the project

--- x-login/src/App.js
+++ x-login/src/App.js
@@ -1,40 +1,9 @@
 import React, { useState } from "react";
 
-const Form = ({ username, password, handleChange, handleSubmit }) => {
-  return (
-    <form onSubmit={handleSubmit} className="myForm">
-      <label>
-        Username:{" "}
-        <input
-          type="text"
-          name="username"
-          value={username}
-          placeholder="username"
-          onChange={handleChange}
-          required
-        />
-      </label>
-      <br />
-      <label>
-        Password:{" "}
-        <input
-          type="password"
-          name="password"
-          value={password}
-          placeholder="password"
-          onChange={handleChange}
-          required
-        />
-      </label>
-      <br />
-      <button>Submit</button>
-    </form>
-  );
-};
 function App() {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
-  const [welcomeText, setWelcomeText] = useState("");
+  const [toggleForm, settoggleForm] = useState(false);
   const [loginSuccess, setLoginSuccess] = useState(false);
 
   const handleChange = (e) => {
@@ -45,26 +14,53 @@ function App() {
   const handleSubmit = (e) => {
     e.preventDefault();
     // console.log(username, password);
-    if (username === "user" && password === "password") {
-      setWelcomeText("Welcome, user!");
-      setLoginSuccess(true);
-    } else setWelcomeText("Invalid username or password");
+    if (!validateInput(username, password)) return;
+
+    if (username !== "user" && password !== "password") setLoginSuccess(true);
+    if (username === "user" && password === "password") settoggleForm(true);
+  };
+
+  const validateInput = (user, password) => {
+    if (user.trim === "" || user === undefined) {
+      return false;
+    }
+    if (password.trim === "" || password === undefined) {
+      return false;
+    }
+    return true;
   };
 
   return (
     <div className="App">
       <h1>Login Page</h1>
-      {loginSuccess ? (
-        <p>{welcomeText}</p>
+      {toggleForm ? (
+        <p>Welcome, user!</p>
       ) : (
         <>
-          <p>{welcomeText}</p>
-          <Form
-            username={username}
-            password={password}
-            handleChange={handleChange}
-            handleSubmit={handleSubmit}
-          />
+          {loginSuccess && <p>Invalid username or password</p>}
+          <form onSubmit={handleSubmit} className="myForm">
+            <label htmlFor="username">Username:</label>
+            <input
+              type="text"
+              name="username"
+              value={username}
+              placeholder="username"
+              onChange={handleChange}
+              required
+            />
+            <br />
+            <label htmlFor="password">Password:</label>
+            <input
+              type="password"
+              name="password"
+              value={password}
+              placeholder="password"
+              onChange={handleChange}
+              required
+            />
+            <br />
+            <button>Submit</button>
+          </form>
         </>
       )}
     </div>
