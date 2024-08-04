6e200fe126bab2e2144feca2d0e09663d05e7e0f
Susovan <susovan.sahoo777@gmail.com>
2024-08-04 19:00:47 +0530

    Added starting files for a new Buildout X-Dictionary

---
+++ x-dictionary/src/App.js
@@ -0,0 +1,22 @@
+
+import { useState } from "react";
+
+const data = [
+  {
+    word: "React",
+    meaning: "A JavaScript library for building user interfaces.",
+  },
+  { word: "Component", meaning: "A reusable building block in React." },
+  { word: "State", meaning: "An object that stores data for a component." },
+];
+
+function App() {
+  
+  return (
+    <div className="App">
+      
+    </div>
+  );
+}
+
+export default App;
