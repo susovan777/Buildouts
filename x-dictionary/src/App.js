eac533fa59cb8f950152bcec265a6e42c7595a11
Susovan <susovan.sahoo777@gmail.com>
2024-08-04 21:25:25 +0530

    Completed the project deployment

--- x-dictionary/src/App.js
+++ x-dictionary/src/App.js
@@ -1,4 +1,3 @@
-
 import { useState } from "react";
 
 const data = [
@@ -11,10 +10,36 @@ const data = [
 ];
 
 function App() {
-  
+  const [input, setInput] = useState("");
+  const [meaning, setMeaning] = useState([]);
+
+  const handleChange = (e) => {
+    setInput(e.target.value);
+  };
+
+  const handleSearch = () => {
+    for (const obj of data) {
+      if (obj.word.toLowerCase() === input.toLowerCase()) {
+        setMeaning(obj.meaning);
+        return;
+      } else setMeaning("Word not found in the dictionary.");
+    }
+  };
+
+  // console.log(meaning);
+
   return (
     <div className="App">
-      
+      <h1>Dictionary App</h1>
+      <input
+        type="text"
+        value={input}
+        placeholder="Search for a word..."
+        onChange={handleChange}
+      />
+      <button onClick={handleSearch}>Search</button>
+      <h4>Definition:</h4>
+      <p>{meaning}</p>
     </div>
   );
 }
